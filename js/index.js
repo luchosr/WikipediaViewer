02d564f29a672e695198bf944da3b75e0a05fb5c
Luciano Ramello <luchosr@gmail.com>
2017-06-07 19:06:24 -0300

    Improved code organization

--- js/index.js
+++ js/index.js
@@ -1,37 +1,33 @@
-console.log("funciona");
-$(document).ready(function() {
-  $('#barraBusqueda').keypress(function(e){
-        if(e.which == 13){//Enter key pressed
-            $('#search').click();//Trigger search button click event
-        }
-    });
-  $("#search").click(function() {
+// Modularized Script.
 
-    var setItem = $("#barraBusqueda").val();
-    console.log(setItem);
-    var url ="https://crossorig.in/https://en.wikipedia.org/w/api.php?action=opensearch&search=" + setItem + "&format=json&callback=?";
-    console.log(url);
-      $('#output').html("<div></div>");
-    $.ajax({
-      method: "GET",
-      url: url,
-      async: false,
-        dataType: "jsonp",
-        success: function(data) {
-          for (var i in data[1]) {
-            if (data[3][i] !== "") {
-              $('#output').append("<div class='item'><a href=" + data[3][i] + " target='blank' >" + data[1][i] + "</a> <br>" + "<p>"+data[2][i]+"</p></div>");
-            }
-              $(".item").fadeIn(200);
+(function(){
 
+  // Executing the script one the DOM is fully loaded.
+
+  $(document).ready(function() {
+
+    $('#barraBusqueda').keypress(function(e){
+          if(e.which == 13){//Enter key pressed
+              $('#search').click();//Trigger search button click event
           }
-         console.log(data[1]);
-         console.log(data[2]);
-         console.log(data[3]);
-        //console.log(data);
-      }
+      });
+      // Search function
+    $("#search").click(function() {
+      var setItem = $("#barraBusqueda").val();//Getting the input value
+      var url ="https://crossorig.in/https://en.wikipedia.org/w/api.php?action=opensearch&search=" + setItem + "&format=json&callback=?";//Setting the API url.
+        $('#output').html("<div></div>");//Cleaning the output.
+      $.ajax({//Getting the JSON from the API, parsed on this case.
+        method: "GET",
+        url: url,
+        async: false,
+          dataType: "jsonp",
+          success: function(data) {
+            for (var i in data[1]) {//For every Item in data
+                $('#output').append("<div class='item'><a href=" + data[3][i] + " target='blank' >" + data[1][i] + "</a> <br>" + "<p>"+data[2][i]+"</p></div>");//Adding the item tittle as an anchor, and the description as a paragraph
+                $(".item").fadeIn(200);//Shwing the item with a 200 ms delay.
+            }
+        }
+      });
     });
   });
-
-
-});
+})();
